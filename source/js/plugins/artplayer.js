6d4b4466437f90b672a908bdab64643cb4300b28
abc1763613206 <abc1763613206@163.com>
2024-09-15 01:55:09 +0800

    feat: 切换字幕隐藏与显示按钮

--- source/js/plugins/artplayer.js
+++ source/js/plugins/artplayer.js
@@ -1,5 +1,28 @@
 (() => {
     const ArtplayerWrapper = {
+        injectSubtitleSwitch: function(artplayer) {
+            if(artplayer.option.subtitle.url != ""){
+                artplayer.controls.add(
+                    {
+                        name: 'subtitle-switch',
+                        index: 10,
+                        position: 'right',
+                        html: '<i class="art-icon toggle-subtitle"><svg width="22" height="22" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg"><path d="M40.031 28.031v-4.031h-20.063v4.031h20.063zM40.031 36v-4.031h-8.063v4.031h8.063zM28.031 36v-4.031h-20.063v4.031h20.063zM7.969 24v4.031h8.063v-4.031h-8.063zM40.031 7.969q1.594 0 2.766 1.219t1.172 2.813v24q0 1.594-1.172 2.813t-2.766 1.219h-32.063q-1.594 0-2.766-1.219t-1.172-2.813v-24q0-1.594 1.172-2.813t2.766-1.219h32.063z"></path></svg></i>',
+                        tooltip: '显示/隐藏字幕',
+                        stat: true,
+                        style: {
+                            color: 'white',
+                        },
+                        click: function (item) {
+                            artplayer.notice.show = item.stat ? '隐藏字幕' : '显示字幕';
+                            artplayer.subtitle.show = !item.stat;
+                            item.stat = !item.stat;
+                            return item;
+                        },
+                    }
+                )
+            }
+        },
         init: function() {
             let artplayers = {}
             const els = document.getElementsByClassName('ds-artplayer');
@@ -8,14 +31,21 @@
                 const artplayer_id = el.getAttribute('artplayer-id')
                 try {
                     artplayers[artplayer_id] = new Artplayer(JSON.parse(atob(el.getAttribute('artplayer-config'))));
+                    artplayers[artplayer_id].on('ready', () => {
+                        this.injectSubtitleSwitch(artplayers[artplayer_id]);
+                    });
                 } catch (e) {
                     console.error(e)
                     if (e instanceof ReferenceError) {
                         setTimeout(() => {
                             artplayers[artplayer_id] = new Artplayer(JSON.parse(atob(el.getAttribute('artplayer-config'))));
+                            artplayers[artplayer_id].on('ready', () => {
+                                this.injectSubtitleSwitch(artplayers[artplayer_id]);
+                            });
                         }, 100)
                     } 
                 }
+                
             }
             const videos = document.getElementsByTagName('video');
             for (let i = 0; i < videos.length; i++) {
